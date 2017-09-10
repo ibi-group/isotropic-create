edab33564c0da21fe1cf56c73802f4a423b2c247
Steven Olmsted <steven.olmsted@ibigroup.com>
2017-09-10 12:43:26 -0400

    Update package

--- test/create.js
+++ test/create.js
@@ -1,16 +1,9 @@
-import {
-    describe,
-    it
-} from 'mocha';
-
+import chai from 'chai';
 import create from '../js/create.js';
+import mocha from 'mocha';
 
-import {
-    expect
-} from 'chai';
-
-describe('create', () => {
-    it('should create an object with the given constructor function and prototype object', () => {
+mocha.describe('create', () => {
+    mocha.it('should create an object with the given constructor function and prototype object', () => {
         const ConstructorFunction = () => void null,
             prototypeObject = {
                 a: 'a',
@@ -20,11 +13,11 @@ describe('create', () => {
 
             object = create(ConstructorFunction, prototypeObject);
 
-        expect(object).to.be.an('object');
-        expect(object).to.have.property('a', 'a');
-        expect(object).to.have.property('b', 'b');
-        expect(object).to.have.property('c', 'c');
-        expect(object).to.have.property('constructor', ConstructorFunction);
-        expect(Reflect.getPrototypeOf(object)).to.equal(prototypeObject);
+        chai.expect(object).to.be.an('object');
+        chai.expect(object).to.have.property('a', 'a');
+        chai.expect(object).to.have.property('b', 'b');
+        chai.expect(object).to.have.property('c', 'c');
+        chai.expect(object).to.have.property('constructor', ConstructorFunction);
+        chai.expect(Reflect.getPrototypeOf(object)).to.equal(prototypeObject);
     });
 });
